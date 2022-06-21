b8fac642b0db462e8783edb1efe6ede560ca2fb6
Mehulpatangya <mehuljainind@gmail.com>
2022-06-22 03:31:12 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Bankoffers from './components/Bankoffers';
+
+function App() {
+  return (
+    <div className="App">
+     big basket
+     <Bankoffers/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Bankoffers.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Bankoffers() {
+  return (
+    <div>Bankoffers</div>
+  )
+}
+
+export default Bankoffers
